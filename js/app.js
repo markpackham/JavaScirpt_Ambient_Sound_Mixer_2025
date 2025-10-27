d7283abd4267ce829fda84995a7c061e10061f1e
M <markpackham1@gmail.com>
2025-10-27 16:08:01 +0000

    Load rain.mp3
    https://youtu.be/e-hGCPaRZYk?t=1571

--- js/app.js
+++ js/app.js
@@ -1,29 +1,29 @@
-import {sounds, defaultPresets} from "./soundData.js";
-import {SoundManger} from "./soundManager.js"
+import { sounds, defaultPresets } from "./soundData.js";
+import { SoundManger } from "./soundManager.js";
 
 class AmbientMixer {
-    // Init the dependencies & default state
-    constructor() {
-        this.soundManger = new SoundManger();
-        this.ui = null;
-        this.presetManger = null;
-        this.timer = null;
-        this.currentSoundState = {};
-        this.isInitialized = false;
-    }
+  // Init the dependencies & default state
+  constructor() {
+    this.soundManger = new SoundManger();
+    this.ui = null;
+    this.presetManger = null;
+    this.timer = null;
+    this.currentSoundState = {};
+    this.isInitialized = false;
+  }
 
-    init() {
-        try {
-            console.log("Init app...");
-            this.isInitialized = true;
-        } catch (error) {
-            console.error("Failed to init app: " + error);
-        }
+  init() {
+    try {
+      this.soundManger.loadSound("rain", "audio/rain.mp3");
+      this.isInitialized = true;
+    } catch (error) {
+      console.error("Failed to init app: " + error);
     }
+  }
 }
 
 // Init app when DOM is ready
 document.addEventListener("DOMContentLoaded", () => {
-    const app = new AmbientMixer();
-    app.init();
-})
+  const app = new AmbientMixer();
+  app.init();
+});
--- js/soundManager.js
+++ js/soundManager.js
@@ -1,13 +1,24 @@
 export class SoundManger {
-    constructor() {
-        this.audioElements = new Map();
-        this.isPlaying = false;
-        console.log("SoundManger created");
-    }
+  constructor() {
+    this.audioElements = new Map();
+    this.isPlaying = false;
+    console.log("SoundManger created");
+  }
 
-    // Load a sound file
-    loadSound(soundId, filePath){
-        console.log(`Loading sound: ${soundId} from ${filePath}`);
-        return true;
+  // Load a sound file
+  loadSound(soundId, filePath) {
+    try {
+      const audio = new Audio();
+      audio.src = filePath;
+      audio.loop = true;
+      audio.preload = "metadata";
+
+      // Add sound to audio elements map
+      this.audioElements.set(soundId, audio);
+      return true;
+    } catch (error) {
+      console.error(`Failed to load sound ${soundId}`);
+      return false;
     }
+  }
 }
