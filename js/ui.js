895503bca61010460d3aefed0c1ebb3d247e62c9
M <markpackham1@gmail.com>
2025-10-30 17:04:21 +0000

    Finish off init() for UI class
    https://youtu.be/e-hGCPaRZYk?t=2980

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,35 @@
 export class UI {
   constructor() {
     this.soundCardsContainer = null;
+    this.masterVolumeSlider = null;
+    this.masterVolumeValue = null;
+    this.playPauseButton = null;
+    this.resetButton = null;
+    this.modal = null;
+    this.customPresetsContainer = null;
+    this.timerDisplay = null;
+    this.timerSelect = null;
+    this.themeToggle = null;
   }
 
-  init() {}
+  init() {
+    this.soundCardsContainer = document.querySelector(".grid");
+    this.masterVolumeSlider = document.getElementById("masterVolume");
+    this.masterVolumeValue = document.getElementById("masterVolumeValue");
+    this.playPauseButton = document.getElementById("playPauseAll");
+    this.resetButton = document.getElementById("resetAll");
+    this.modal = document.getElementById("savePresetModal");
+    this.customPresetsContainer = document.getElementById("customPresets");
+    this.timerDisplay = document.getElementById("timerDisplay");
+    this.timerSelect = document.getElementById("timerSelect");
+    this.themeToggle = document.getElementById("themeToggle");
+  }
+
+  // Create sound card HTML
+  createSoundCards(sound) {
+    const card = document.createElement("div");
+    card.className =
+      "sound-card bg-white/10 backdrop-blur-md rounded-2xl p-6 relative overflow-hidden transition-all duration-300";
+    card.dataset.sound = sound.id;
+  }
 }
