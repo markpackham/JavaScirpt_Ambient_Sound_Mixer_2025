c9d24cff1231c8a6787a5b1837ac1c0817612139
M <markpackham1@gmail.com>
2025-11-06 18:32:53 +0000

    Add stop timer method
    https://youtu.be/e-hGCPaRZYk?t=11644

---
+++ js/timer.js
@@ -0,0 +1,54 @@
+export class Timer {
+  constructor(onComplete, onTick) {
+    this.duration = 0;
+    this.remaining = 0;
+    this.intervalId = null;
+    this.onComplete = onComplete;
+    this.onTick = onTick;
+    this.isRunning = false;
+  }
+
+  // Start timer with duration in minutes
+  start(minutes) {
+    if (minutes <= 0) {
+      this.stop();
+      return;
+    }
+
+    // Convert to seconds
+    this.duration = minutes * 60;
+    this.remaining = this.duration;
+    this.isRunning = true;
+
+    // Clear any existing interval
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+
+    // Update display
+    this.updateDisplay();
+
+    // Start countdown - 1000 milliseconds is 1 second
+    this.intervalId = setInterval(() => {
+      this.remaining--;
+      this.updateDisplay();
+
+      if (this.remaining <= 0) {
+        this.complete();
+      }
+    }, 1000);
+  }
+
+  // Stop timer
+  stop() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+
+    this.duration = 0;
+    this.remaining = 0;
+    this.isRunning = false;
+    this.updateDisplay();
+  }
+}
